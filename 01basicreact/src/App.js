d2a8f35c334670b2d0d69e494502977e6886bcd0
Anshu kumar <kumaranshu303393@gmail.com>
2024-12-25 13:13:52 +0530

    React and react with vite complted with a project 😊

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Chai or React | Anshu</h1>
+      <div>React Series</div>
+      <ul>
+        <li>Why React</li>
+      </ul>
+      <h5>It is easy to manage and build complex frontends</h5>
+      <ul>
+        <li>When React</li>
+      </ul>
+      <h5>After mastering JavaScript</h5>
+      <ul>
+        <li>React is a library (Framework as military and library as cool dude)</li>
+        <li>
+          React does not have router, state management while it uses React-Router-DOM and Redux,
+          Redux Toolkit, Zustand, Context API.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  
+
+  return (
+  <h1>Chai or recat with vite | Anshu</h1>
+  )
+}
+
+export default App
