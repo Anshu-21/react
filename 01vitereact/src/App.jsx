74a9dee034eaa9480c80bb6d6dda077db1846897
Anshu kumar <kumaranshu303393@gmail.com>
2024-12-26 01:25:55 +0530

    lecture 4 is completed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,11 @@ import Chai from "./chai"
 
 function App() {
   
-
+ const username = "chai aur code"
   return (
   <>
   <Chai/>
-  <h1>Chai or react</h1>
+  <h1>Chai or react | {username}</h1>
   <p>Lorem ipsum dolor sit amet.</p>
   </>
   )
